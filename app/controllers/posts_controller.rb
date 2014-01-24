a07eac739df9dd1a37bc9ae272104197fa39265d
Jevon Wright <jevon@jevon.org>
2014-01-24 14:32:53 +1300

    implementing post edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,20 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
 	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
 		@post = Post.find(params[:id])
 	end
 
